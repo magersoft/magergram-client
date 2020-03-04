e1e9e2d40fc51c9051af9d1fcf1d8ea8f7b353a0
v.mager <v.mager@b2b-center.ru>
2020-03-05 01:36:22 +0300

    added i18n

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from './locale/en_EN';
+import translationRU from './locale/ru_RU';
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  ru: {
+    translation: translationRU
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false
+    }
+});
+
+export default i18n;
---
+++ src/locale/en_EN/index.js
@@ -0,0 +1,4 @@
+import messages from './messages';
+import UI from './UI';
+
+export default { ...UI, ...messages }
